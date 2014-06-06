7a7a8644c78641b6f63bcdcb1d662f52a864b6e3
Paul Cooley <pcooley@gmail.com>
2014-06-05 21:45:23 -0700

    A basic test - templating

---
+++ server/readability_svr.py
@@ -0,0 +1,31 @@
+from bottle import route, run
+import sys
+import os
+import barrister
+
+sys.path.append('./')      # import the root directory
+class ReadabilityService(object):
+
+    def readability(self, request):
+        print request
+        return request
+
+@route('/readability')
+def read():
+        resp_data = server.call_json(request.data)
+        resp = make_response(resp_data)
+        resp.headers['Content-Type'] = 'application/json'
+        return resp
+
+@route('/hello')
+def hello():
+    return "Hello World!"
+
+fn = os.path.join(os.path.dirname(__file__), './idl/readability.json')
+contract = barrister.contract_from_file(fn)
+server = barrister.Server(contract)
+server.add_handler("ReadabilityService", ReadabilityService())
+
+
+if __name__ == "__main__":
+    run(host='localhost', port=8080, debug=True)
--- server/test/test_readability-svr.py
+++ server/test/test_readability-svr.py
@@ -1,4 +1,19 @@
+import sys
+import unittest
+
+sys.path.append('..')      # import the root directory
+import readability_svr
 
 def test():
     return True;
 
+class Simple(unittest.TestCase):
+
+#    def setUp(self):
+#        self.seq = range(10)
+
+    def test_hello(self):
+        self.assertEqual(readability_svr.hello(),"Hello World!")
+
+if __name__ == '__main__':
+    unittest.main()
