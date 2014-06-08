cd2610829a040f6099ad069173688072b5ff89f8
Paul Cooley <pcooley@gmail.com>
2014-06-08 06:50:48 -0700

    Shell of a Single Page App for the UI

---
+++ ui/js/app/main.js
@@ -0,0 +1,13 @@
+//Load Web App JavaScript Dependencies/Plugins
+define([
+    "jquery",
+//    "modernizr",
+    "underscore",
+    "backbone",
+    "bootstrap"
+], function()
+{
+    //do stuff
+    console.log('required plugins loaded...');
+    $('#main').append("<p> Hello World </p>");
+});
