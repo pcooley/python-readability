b2e3dd035260af4d059508600629a0dc392a2cb3
John Helm <jhelm@jhelmassociates.com>
2014-07-24 21:20:30 -0700

    July 24th pairing session.  New contract and fun.

--- server/service/ReadabilityService.py
+++ server/service/ReadabilityService.py
@@ -3,6 +3,6 @@
 class ReadabilityService(object):
 
     def readability(self, req):
-        stringlength = len(req["text"])
-        response = { "length":"%s" %(stringlength)}
+        stringlength = len(req["reqData"])
+        response = { "resultCode":0, "docScore":0, "lineScores": [{"lineNo":0, "lineTxt":"one", "lineScore":42}]}
         return response
