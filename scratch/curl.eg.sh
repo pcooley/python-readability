f322eb62dd64c04b07494ece74bc1fbcc993fe6c
John Helm <jhelm@jhelmassociates.com>
2015-01-29 21:00:11 -0800

    A number of re-boot updates

--- scratch/curl.eg.sh
+++ scratch/curl.eg.sh
@@ -1,1 +1,1 @@
-curl -H "Content-Type:application/json" -X POST -d @postdata.json http://localhost:8080/readability
+curl -H "Content-Type:application/json" -X POST -d @postdata.json http://127.0.0.1:8080/readability
--- server/service/ReadabilityService.py
+++ server/service/ReadabilityService.py
@@ -4,5 +4,14 @@ class ReadabilityService(object):
 
     def readability(self, req):
         stringlength = len(req["reqData"])
-        response = { "resultCode":0, "docScore":0, "lineScores": [{"lineNo":0, "lineTxt":"one", "lineScore":42}]}
+        wc = self.wordcount(req["reqData"])
+        response = { "resultCode":0, "docScore":0, "characterCount": stringlength,
+                     "wordCount": wc, "lineScores": [{"lineNo":0, "lineTxt":"one", "lineScore":42}]}
         return response
+
+    def version(self):
+        return "0.0.1"
+
+    def wordcount(self, str):
+        return len(str.split())
+        
